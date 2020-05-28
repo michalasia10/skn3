f36f76c53400b526a42d3ccc69a09f4abff2eb6a
michalasia10 <misiek1027@o2.pl>
2020-05-28 17:38:16 +0200

    fix settings

--- skn3/settings/base.py
+++ skn3/settings/base.py
@@ -14,7 +14,7 @@ import os
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__) +  "/.."))
 
 
 # Quick-start development settings - unsuitable for production
@@ -149,9 +149,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, '../../static')
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = (
-#      os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+     os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '../../media')
