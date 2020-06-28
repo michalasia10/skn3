748946c1a32887a527e13d7804326e3849a17ec7
michalasia10 <misiek1027@o2.pl>
2020-06-28 02:49:41 +0200

    production.py

--- skn3/settings/production.py
+++ skn3/settings/production.py
@@ -3,3 +3,20 @@ import dj_database_url
 
 DEBUG = False
 DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
