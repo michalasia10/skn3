fa03642ed8069864bc51f3c383094828bd2ed370
michalasia10 <misiek1027@o2.pl>
2020-06-29 00:40:22 +0200

    staticfiles

--- skn3/settings/base.py
+++ skn3/settings/base.py
@@ -153,6 +153,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, '../../static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
      os.path.join(BASE_DIR,'static'),
+    '/Project/skn3/static/',
 )
 
 MEDIA_URL = '/media/'
