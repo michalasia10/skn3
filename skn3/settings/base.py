d379321883fc30209d366c9fe1f18604c2915639
michalasia10 <misiek1027@o2.pl>
2020-06-29 01:06:40 +0200

    staticfiles

--- skn3/settings/base.py
+++ skn3/settings/base.py
@@ -157,7 +157,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 MEDIA_URL = '/media/'
