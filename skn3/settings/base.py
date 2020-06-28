4f3975bf09f517c54449aca3075a7db1f1331355
michalasia10 <misiek1027@o2.pl>
2020-06-29 01:04:27 +0200

    staticfiles

--- skn3/settings/base.py
+++ skn3/settings/base.py
@@ -15,8 +15,8 @@ import dj_database_url
 import psycopg2
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__) +  "/.."))
-
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__) +  "/.."))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -149,10 +149,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 # STATIC_ROOT = os.path.join(BASE_DIR, '../../static')
-STATIC_ROOT = (os.path.join(os.path.dirname(__file__), '..', 'static'))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-     os.path.join(BASE_DIR,'static')
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # ADMIN_MEDIA_PREFIX = '/static/admin/'
