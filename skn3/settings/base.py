9af86d4eddf49bbd3cb98c7dac739e25da30ca0e
michalasia10 <misiek1027@o2.pl>
2020-07-03 00:04:14 +0200

    static for admin v9

--- skn3/settings/base.py
+++ skn3/settings/base.py
@@ -73,6 +73,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+
 ]
 
 ROOT_URLCONF = 'skn3.urls'
